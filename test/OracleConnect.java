e78c4853d8bdbae89cffeaaf4954f4b8999f75fb
nitinkaveriappa <nitinkaveriappa@gmail.com>
2016-11-17 17:51:50 -0500

    Removed sensitive data

--- test/OracleConnect.java
+++ test/OracleConnect.java
@@ -1,38 +1,36 @@
 package test;
 
-import java.sql.*;
+import java.sql.*;  
 
 public class OracleConnect {
-
-	public static void main(String args[]){
-
+	
+	public static void main(String args[]){ 
+		
 		String user;
 		String password;
 		user = "nudiyand";
 		password = "";
 
-		try{
-		//step1 load the driver class
-		Class.forName("oracle.jdbc.driver.OracleDriver");
-		//step2 create  the connection object
+		try{  
+		//step1 load the driver class  
+		Class.forName("oracle.jdbc.driver.OracleDriver");  
+		//step2 create  the connection object  
 		Connection con = DriverManager.getConnection("jdbc:oracle:thin:@apollo.ite.gmu.edu:1521:ite10g", user, password);
-		String sql = "SELECT mid,name FROM member WHERE age = ?";
+		String sql = "SELECT id,name FROM customer WHERE id = ?"; 
 		//step3 create the statement object
 		PreparedStatement prepStmt = con.prepareStatement(sql);
-		int age = 17;
-		prepStmt.setInt(1, age);
-        	ResultSet rs = prepStmt.executeQuery();
-       	 	//step4 execute query
-
-		while(rs.next())
-		System.out.println(rs.getString(1)+"  "+rs.getString(2));
-
-		//step5 close the connection object
+		int id = 1;
+		prepStmt.setInt(1, id); 
+        ResultSet rs = prepStmt.executeQuery();
+        //step4 execute query  
+		  
+		while(rs.next())  
+		System.out.println(rs.getString(1)+"  "+rs.getString(2));  
+		  
+		//step5 close the connection object  
 		con.close();
 		}
 		catch(Exception e)
-		{
-			System.out.println(e);
-		} 
+		{ System.out.println(e);} 
 	}
 }
