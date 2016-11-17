f6306c9a6319a683144e48119edf769662633eb2
nitinkaveriappa <nitinkaveriappa@gmail.com>
2016-11-17 17:43:54 -0500

    View table content update

---
+++ real/DBConnect.java
@@ -0,0 +1,24 @@
+package real;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnect {
+
+	static Connection connect() {
+		String user;
+		String password;
+		user = "nudiyand";
+		password = "";
+		Connection con=null;
+		try{  
+		//step1 load the driver class  
+		Class.forName("oracle.jdbc.driver.OracleDriver");  
+		//step2 create  the connection object  
+		con = DriverManager.getConnection("jdbc:oracle:thin:@apollo.ite.gmu.edu:1521:ite10g", user, password);
+		}
+		catch(Exception e)
+		{ System.out.println(e);} 
+		return con;
+	}
+}
--- real/SimpleAuction.java
+++ real/SimpleAuction.java
@@ -1,26 +1,47 @@
 package real;
 
 import java.io.*;
+import java.sql.*;;
 
 public class SimpleAuction {
 
-	public static void main(String args[])
-	{
-    System.out.println("Welcome to SimpleAuction Database CLI");
-    System.out.println("Please select one of the options below:");
-
-    System.out.println("1. View table content");
-    System.out.println("2. Add/Modify records");
-    System.out.println("3. Search database");
-		System.out.println("User Input(1-3): ");
-
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		try {
-			Str = br.readLine();
-		} catch (IOException e) {
-			e.printStackTrace();
+	public static void main(String args[]) {
+		String Op1 = null;
+		int Int1 = 0;
+		while(Int1!=4){
+		    System.out.println("Welcome to SimpleAuction Database CLI");
+		    System.out.println("Please select one of the options below:");
+		
+		    System.out.println("1. View table content");
+		    System.out.println("2. Add/Modify records");
+		    System.out.println("3. Search database");
+		    System.out.println("4. Exit");
+			System.out.println("User Input(1-4): ");
+	
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			try {
+				Op1 = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			Int1 = Integer.parseInt(Op1);
+			System.out.println(Int1);
+			
+			switch(Int1){
+			case 1:
+			Connection conn = DBConnect.connect();
+	
+			String sql = "SELECT table_name FROM user_tables"; 
+			PreparedStatement prepStmt = null;
+				try {
+					prepStmt = conn.prepareStatement(sql);
+					ResultSet rs = prepStmt.executeQuery();
+					while(rs.next())  
+						System.out.println(rs.getString(1));  
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}   
+			}
 		}
-		Int = Integer.parseInt(Str);
-		System.out.println(Int);
 	}
 }
